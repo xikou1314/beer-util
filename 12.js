4d92366e33716304e7dc0e2d993aacfcd854ffa6
JohnY <xikou1314@126.com>
2020-04-03 17:58:38 +0800

    save: 20200403

--- 12.js
+++ 12.js
@@ -40,4 +40,23 @@ function BST() {
             }
         }
     }
+
+    function inOrder(node) {
+        if(!(node == null)) {
+            inOrder(node.left);
+            console.log(node.show() + " ");
+            inOrder(node.right);
+        }
+    }
 }
+
+var nums = new BST();
+nums.insert(23);
+nums.insert(45);
+nums.insert(16);
+nums.insert(37);
+nums.insert(3);
+nums.insert(99);
+nums.insert(22);
+console.log("Inorder traversal:");
+nums.inOrder(nums.root);
