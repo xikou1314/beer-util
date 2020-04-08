5d6ce217031ef8e8efe5187d02c971afb152c394
yh <xikou1314@126.com>
2020-04-08 22:05:19 +0800

    save: 4/8

--- 16.js
+++ 16.js
@@ -7,30 +7,33 @@ function CArray(numElements) {
     this.clear = clear;
     this.setData = setData;
     this.swap = swap;
+    this.bubbleSort = bubbleSort;
+    this.selectionSort = selectionSort;
+    this.insertionSort = insertionSort;
 
-    for (var i=0; i < numElements; ++i) {
+    for (var i = 0; i < numElements; ++i) {
         this.dataStore[i] = i;
     }
 
     function setData() {
-        for ( var i = 0; i < this.numElements; ++i ) {
+        for (var i = 0; i < this.numElements; ++i) {
             this.dataStore[i] = Math.floor(Math.random() * (this.numElements + 1));
         }
     }
 
     function clear() {
-        for (var i=0; i<this.dataStore.length; ++i) {
+        for (var i = 0; i < this.dataStore.length; ++i) {
             this.dataStore[i] = 0;
         }
     }
 
     function insert(element) {
-        this.dataStore[this.pos ++ ] = element;
+        this.dataStore[this.pos++] = element;
     }
 
     function toString() {
         var retstr = "";
-        for (var i=0; i < this.dataStore.length; ++i) {
+        for (var i = 0; i < this.dataStore.length; ++i) {
             retstr += this.dataStore[i] + " ";
             if (i > 0 && i % 10 == 0) {
                 retstr += '\n';
@@ -44,10 +47,64 @@ function CArray(numElements) {
         arr[index1] = arr[index2];
         arr[index2] = temp;
     }
+    // 冒泡排序
+    function bubbleSort() {
+        var numElements = this.dataStore.length;
+        for (var outer = numElements; outer >= 2; --outer) {
+            for (var inner = 0; inner <= outer - 1; ++inner) {
+                if (this.dataStore[inner] > this.dataStore[inner + 1]) {
+                    swap(this.dataStore, inner, inner + 1);
+                }
+            }
+        }
+    }
+    // 选择排序
+    function selectionSort() {
+        var min;
+        for (var outer = 0; outer <= this.dataStore.length - 2; ++outer) {
+            min = outer;
+            for (var inner = outer + 1; inner <= this.dataStore.length - 1; ++inner) {
+                if (this.dataStore[inner] < this.dataStore[min]) {
+                    min = inner;
+                }
+            }
+            swap(this.dataStore, outer, min);
+        }
+    }
+
+    function insertionSort() {
+        var temp, inner;
+        for (var outer = 1; outer <= this.dataStore.length - 1; ++outer) {
+            temp = this.dataStore[outer];
+            inner = outer;
+            while (inner > 0 && (this.dataStore[inner - 1] >= temp)) {
+                this.dataStore[inner] = this.dataStore[inner - 1];
+                --inner;
+            }
+            this.dataStore[inner] = temp;
+        }
+    }
 
 }
 
-var numElements = 100;
-var myNums = new CArray(numElements);
-myNums.setData();
-console.log(myNums.toString());
+var numElements = 100000;
+var nums = new CArray(numElements);
+nums.setData();
+var start = new Date().getTime();
+nums.bubbleSort();
+var stop = new Date().getTime();
+var elapsed = stop - start;
+console.log(" 对 " + numElements + " 个元素执行冒泡排序消耗的时间为：" +
+    elapsed + " 毫秒。");
+start = new Date().getTime();
+nums.selectionSort();
+stop = new Date().getTime();
+elapsed = stop - start;
+console.log(" 对 " + numElements + " 个元素执行选择排序消耗的时间为：" +
+    elapsed + " 毫秒。");
+start = new Date().getTime();
+nums.insertionSort();
+stop = new Date().getTime();
+elapsed = stop - start;
+console.log(" 对 " + numElements + " 个元素执行插入排序消耗的时间为：" +
+    elapsed + " 毫秒。");
